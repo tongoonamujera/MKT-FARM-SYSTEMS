efc3dd36b9b8c606890d78141d7825f607cc76d7
tongoonamujera <tongoonamujera@gmail.com>
2021-08-28 18:05:29 +0200

    Add more changes to users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def self.admin
+    where('is_admin =?', true)
+  end
+
+  def self.farm_owner
+    where('is_farm_owner =?', true)
+  end
+
+  def self.manager
+    where('is_manager =?', true)
+  end
 end
