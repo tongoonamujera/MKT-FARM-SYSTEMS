0d967d457886aef0a0bbd042fa5ad72fa71327d6
tongoonamujera <tongoonamujera@gmail.com>
2021-09-04 21:08:10 +0200

    Add more cahnges

---
+++ app/javascript/src/custom.js
@@ -0,0 +1,8 @@
+  function formDivControll(){
+    var links = document.getElementById('fromDiv');
+    if (links.style.display === 'block'){
+      links.style.display === 'none';
+    }else{
+      links.style.display === 'block';
+    }
+  }
