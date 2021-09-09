4f033372b4086d2546e3573d9c88b3a61bbf540c
tongoonamujera <tongoonamujera@gmail.com>
2021-09-09 10:03:31 +0200

    Add jquery and some changes to code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('src/custom')
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/src/custom.js
+++ app/javascript/src/custom.js
@@ -1,13 +1,32 @@
 
 window.onload = function(){
-  function formDivControll(){
-    var links = document.getElementById("otherDeductions");
-    if (links.style.display === "flex"){
-      links.style.display === "none";
-    }else{
-      links.style.display === "flex";
+  // function formDivControll(){
+  //   var links = document.getElementById("otherDeductions");
+  //   if (links.style.display === "flex"){
+  //     links.style.display === "none";
+  //   }else{
+  //     links.style.display === "flex";
+  //   }
+  // }
+  // a = document.getElementById("clickButton");
+  // a.addEventListener("click", formDivControll)
+
+  var acc = document.getElementsByClassName("clickbutton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-  }
-  a = document.getElementById("clickButton");
-  a.addEventListener("click", formDivControll)
+  });
+}
 }
