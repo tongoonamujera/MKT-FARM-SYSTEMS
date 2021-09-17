e294615f50c8ee18de593f6cba45ae55348114a8
tongoonamujera <tongoonamujera@gmail.com>
2021-09-17 18:22:15 +0200

    Add some changes

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,17 @@
 class Store < ApplicationRecord
   belongs_to :user
+
+  after_commit :update_total_amount, on: :update
+
+  def update_total_amount
+    update_column(:quantity, total_value)
+  end
+
+  def total_value
+    c = Store.last.types
+    d = Store.last.id
+    a = Store.where("types =? AND id =?", c,d).pluck(:quantity, :top_up).flatten.compact.inject(:+)
+
+    a 
+  end
 end
