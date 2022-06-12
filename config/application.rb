4caf0205d29100fc976a3af1350b3f1f27130d33
tongoonamujera <tongoonamujera@gmail.com>
2022-06-12 15:03:29 +0200

    Add react and redux

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module MKTFarmSystems
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+      end
+    end
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 end
